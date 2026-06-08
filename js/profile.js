7006d99edafa49711f33d4c573ba0de39c306755
IrmaTchanturia <irmachanturia1@gmail.com>
2026-06-08 13:43:58 +0400

    update information, change password, delete accaount

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,92 @@
-https://api.everrest.educata.dev/auth/update
+//მონაცემების დარედაქტირება და ცვლილებების შენახვა
+document
+  .getElementById("update-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const userData = {
+      firstName: document.getElementById("update-firstName").value,
+      lastName: document.getElementById("update-lastName").value,
+      age: document.getElementById("update-age").value,
+      email: document.getElementById("update-email").value,
+      phone: document.getElementById("update-phone").value,
+      address: document.getElementById("update-address").value,
+      zipcode: document.getElementById("update-zipcode").value,
+      avatar: document.getElementById("update-avatar").value,
+      gender: document.getElementById("update-gender").value,
+    };
+
+    fetch("https://api.everrest.educata.dev/auth/update", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer " + JSON.parse(sessionStorage.getItem("token"))?.access_token,
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert("Changes saved successfully!");
+        }
+      });
+  });
+
+//პაროლის ცვლილება
+document
+  .getElementById("change-password-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const passwordData = {
+      oldPassword: document.getElementById("current-password").value,
+      newPassword: document.getElementById("new-password").value,
+    };
+
+    fetch("https://api.everrest.educata.dev/auth/change_password", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer " + JSON.parse(sessionStorage.getItem("token"))?.access_token,
+      },
+      body: JSON.stringify(passwordData),
+    })
+      .then((res) => {
+        console.log(res.status);
+        return res.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert("Password changed successfully!");
+        }
+      });
+  });
+
+//ანგარიშის წაშლა
+document.getElementById("delete-form").addEventListener("click", function () {
+  fetch("https://api.everrest.educata.dev/auth/delete", {
+    method: "DELETE",
+    headers: {
+      Authorization:
+        "Bearer " + JSON.parse(sessionStorage.getItem("token"))?.access_token,
+    },
+  })
+    .then((res) => {
+      console.log(res.status);
+      return res.json();
+    })
+    .then((data) => {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        alert("Your Account is Deleted!");
+        window.location.href = "index.html";
+      }
+    });
+});
--- js/userinfo.js
+++ js/userinfo.js
@@ -1,8 +1,6 @@
 let welcomeMessage = document.getElementById("welcome-message");
 
-//current user-ზე წვდომა token-ის საშალებით
 async function getCurrentUser() {
-  // token ყოველთვის აქედან ვიღებთ
   const token = JSON.parse(sessionStorage.getItem("token"))?.access_token;
 
   if (!token) {
@@ -18,10 +16,23 @@ async function getCurrentUser() {
 
   const user = await res.json();
 
-  welcomeMessage.innerHTML = `
-    <p> Welcome, ${user.firstName}!</p>
-  `;
+  if (welcomeMessage) {
+    welcomeMessage.innerHTML = `<p>Welcome, ${user.firstName}!</p>`;
+  }
+
+  if (document.getElementById("update-firstName")) {
+    document.getElementById("update-firstName").value = user.firstName;
+    document.getElementById("update-lastName").value = user.lastName;
+    document.getElementById("update-email").value = user.email;
+    document.getElementById("update-age").value = user.age;
+    document.getElementById("update-address").value = user.address;
+    document.getElementById("update-phone").value = user.phone;
+    document.getElementById("update-zipcode").value = user.zipcode;
+    document.getElementById("update-avatar").value = user.avatar;
+    document.getElementById("update-gender").value = user.gender;
+  }
 
   console.log(user);
 }
+
 getCurrentUser();
